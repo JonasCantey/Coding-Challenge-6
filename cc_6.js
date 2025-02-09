e7e1da6335d54bbfeec389e341f86802760ff01c
Jonas <jonashooncantey@gmail.com>
2025-02-09 17:42:20 -0500

    Task 4 - Subscription Pricing Model

--- cc_6.js
+++ cc_6.js
@@ -24,14 +24,29 @@ console.log("Task 3")
 
 function calculateBonus(salary, performanceRating) {
     let bonus; 
-    if (performanceRating === "Excellent")  {bonus = salary * 0.2}
+    if (performanceRating === "Excellent")  {bonus = salary * 0.2} //Declared that if the performance rating is Excellent, the formula for bonus will be bonus = salary * 0.2
         
-     else if (performanceRating === "Good")  {bonus = salary * 0.1}
-     else if (performanceRating === "Average") {bonus = salary * 0.05} 
+     else if (performanceRating === "Good")  {bonus = salary * 0.1} //Declared that if the performance rating is good, the formula for bonus will be bonus = salary * 0.1
+     else if (performanceRating === "Average") {bonus = salary * 0.05}  //Declared that if the performance rating is average, the formula for bonus will be bonus = salary * 0.05
      else { bonus = 0}
-     return `Bonus: $${bonus.toFixed(2)}`
+     return `Bonus: $${bonus.toFixed(2)}`  //The function will now return the bonus to a rounded hundreths decimal place
     }
 
 console.log(calculateBonus(5000, "Excellent"))
 console.log(calculateBonus(7000, "Good"))
 
+//Task 4
+console.log("Task 4")
+
+function calculateSubscriptionCost(plan, months, discount) {
+    let bonus; 
+    if (plan === "Enterprise")  {cost = (50 * months) - discount } //declared that if the plan is Enterprise, the formula for cost will be cost = (50 * months) - discount
+        
+     else if (plan === "Premium")  {cost = (20 * months) - discount} //declared that if the plan is Premium, the formula for cost will be cost = (20 * months) - discount
+     else if (plan === "Basic") {cost = (10 * months) - discount} //declared that if the plan is Basic, the formula for cost will be cost = (10 * months) - discount
+     else { cost = 0}
+     return `Total cost: $${cost.toFixed(2)}` //This will now return cost rounded to the hundreths place
+    }
+
+console.log(calculateSubscriptionCost("Basic", 6, 10))
+console.log(calculateSubscriptionCost("Premium", 12, 0))
